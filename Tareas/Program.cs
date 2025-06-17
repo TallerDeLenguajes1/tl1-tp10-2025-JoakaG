c13f869770d6afd0384e555eae0ad8ad0e9e1cba
Joaquina Girbau <girbaujoaquina2@gmail.com>
2025-06-17 13:55:23 -0300

    punto 1 comple y parte del 2

---
+++ Tareas/Program.cs
@@ -0,0 +1,32 @@
+﻿using EspacioTareas;
+using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+
+using HttpClient client = new HttpClient();
+
+HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/");
+response.EnsureSuccessStatusCode();
+string responseBody = await response.Content.ReadAsStringAsync();
+List<Tarea> listTareas = JsonSerializer.Deserialize<List<Tarea>>(responseBody);
+foreach (var Tarea1 in listTareas)
+{
+    if (Tarea1.completed == false)
+    {
+        System.Console.WriteLine($"Titulo: {Tarea1.title} \n Completado: {Tarea1.completed} \n --------- \n");
+    }
+}
+foreach (var Tarea1 in listTareas)
+{
+    if (Tarea1.completed)
+    {
+        System.Console.WriteLine($"Titulo: {Tarea1.title} \n Completado: {Tarea1.completed} \n --------- \n");
+    }
+}
+
+var json = JsonSerializer.Serialize(listTareas);
+File.WriteAllText("tareas.json", json);
+
---
+++ Usuarios/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+using HttpClient client = new HttpClient();
+
+HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/users");
+response.EnsureSuccessStatusCode();
+string responseBody = await response.Content.ReadAsStringAsync();
+List<Usuarios> listUsuarios = JsonSerializer.Deserialize<List<Usuarios>>(responseBody);
+
+foreach (var Usuario in listUsuarios)
+{
+System.Console.WriteLine($"ID: {Usuario.id} \n Name: {Usuario.name} \n Email: {Usuario.email} \n Address: \n  Calle {Usuario.address.street} \n  Suite: {Usuario.address.suite} \n  City: {Usuario.address.city} \n  ZipCode: {Usuario.address.zipcode} \n  Geo: \n  Lat: {Usuario.address.geo.lat} \n  Lng: {Usuario.address.geo.lng}");
+    
+}
